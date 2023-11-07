f63e1b933a1a682028d14a3ae28a812bc4c94ff6
benaicha <benaichamohammed@outlook.fr>
2023-11-07 16:56:34 +0100

    17h/07

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-console.log("script chargée ");
+console.log("script chargée !");
 
 
+
+
+    let rue = document.getElementById("rue_facturation");
+
+// Ajouter un écouteur d'événement pour surveiller le clic sur le bouton
+        rue.addEventListener("click", function(){
+            fetch('https://api.exemple.com/adresse') // Remplacez l'URL par l'URL réelle de l'API
+                .then(response => response.json())
+                .then(data => {
+                    let adresseInput = document.getElementById("adresseInput");
+                    adresseInput.value = data.address; // Assurez-vous que la réponse de l'API est correctement traitée ici
+});
